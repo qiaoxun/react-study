042621e17f2715f4450d3b6126014347bb0a238e
Xun Qiao <woqiaoxun@163.com>
2021-02-07 23:11:37 +0800

    todo list

--- todo-list/src/components/Header/index.jsx
+++ todo-list/src/components/Header/index.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
 export default class index extends Component {
+
+  handleKeyUp = (event) => {
+    const {target, keyCode} = event
+    if (keyCode === 13) {
+      const name = target.value
+      this.props.addTodo({name, id: new Date().getTime(), done: false})
+      target.value = ''
+    }
+  }
+
   render() {
     return (
       <div>
-        <input className="enter-input" type="text" placeholder="Please input task name and press enter" />
+        <input onKeyUp={this.handleKeyUp} className="enter-input" type="text" placeholder="Please input task name and press enter" />
       </div>
     )
   }
--- todo-list/src/components/List/index.jsx
+++ todo-list/src/components/List/index.jsx
@@ -3,11 +3,14 @@ import Item from '../Item'
 
 export default class index extends Component {
   render() {
+    const { delTodo, checkOrUnCheck, todos } = this.props
     return (
       <div className="list">
-        <Item name={"Joey"} />
-        <Item name={"Tom"} />
-        <Item name={"Jack"} />
+        {
+          todos.map(l => {
+            return <Item {...l} key={l.id} delTodo={delTodo} checkOrUnCheck={checkOrUnCheck} />
+          })
+        }
       </div>
     )
   }
