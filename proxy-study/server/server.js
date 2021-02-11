164da8bb98f3a12bdb4bc970961eea4908e6ce71
Xun Qiao <woqiaoxun@163.com>
2021-02-11 22:30:40 +0800

    proxy

--- proxy-study/server/server.js
+++ proxy-study/server/server.js
@@ -1,15 +1,19 @@
 http = require('http')
 
 http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
+  if (req.url === '/cars') {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    const cars = [
+      {brand: 'Mazida', name: 'Mazida3'},
+      {brand: 'BMW', name: '325li'},
+      {brand: 'Honda', name: 'Civic'},
+    ]
 
-  const cars = [
-    {brand: 'Mazida', name: 'Mazida3'},
-    {brand: 'BMW', name: '325li'},
-    {brand: 'Honda', name: 'Civic'},
-  ]
-
-  res.end(JSON.stringify(cars))
+    res.end(JSON.stringify(cars))
+  } else {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end('OK')
+  }
 }).listen(9007)
 
 console.log('Server is running on port 9007 ...')
