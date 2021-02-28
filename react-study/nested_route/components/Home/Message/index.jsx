189f96613a0025db3f86b67f34b4eec72bc89a77
Xun Qiao <woqiaoxun@163.com>
2021-02-28 15:52:50 +0800

    nested route

--- react-study/nested_route/components/Home/Message/index.jsx
+++ react-study/nested_route/components/Home/Message/index.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class index extends Component {
   render() {
     return (
-      <h1>This is Test</h1>
+      <div className="tab-pane fade show active">
+        Message
+      </div>
     )
   }
 }
---
+++ react-study/src/components/Home/Message/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+      <div className="tab-pane fade show active">
+        Message
+      </div>
+    )
+  }
+}
